d6098371891b7d3d540c217ad0cf71c1a28d1116
Shubrato Debnath <shubratodn44985@gmail.com>
2023-04-22 00:14:12 +0600

    All Category DONE

--- src/main/java/com/onneshon/blog/controllers/CategoryControllers.java
+++ src/main/java/com/onneshon/blog/controllers/CategoryControllers.java
@@ -1,8 +1,12 @@
 package com.onneshon.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.onneshon.blog.payloads.ApiResponse;
 import com.onneshon.blog.payloads.CategoryDto;
 import com.onneshon.blog.services.CategoryServices;
 
@@ -28,10 +33,35 @@ public class CategoryControllers {
 	}
 	
 	//update category
-	@PutMapping("/category/{catId}")
+	@PutMapping("/{catId}")
 	public ResponseEntity<?> updateCategory(@RequestBody CategoryDto catDto, @PathVariable("catId") int catId){
 		CategoryDto updateCategory = catServices.updateCategory(catDto, catId);
 		return new ResponseEntity<>(updateCategory, HttpStatus.CREATED);
+	}
+	
+	
+	//Delete category
+	@DeleteMapping("/{catId}")
+	public ResponseEntity<?> updateCategory(@PathVariable("catId") int catId){
+		catServices.deleteCategory(catId);
+		ApiResponse response = new ApiResponse("DeleteCategory", true, "Category Deleted");
+		return ResponseEntity.ok(response);
+	}
+	
+	
+	//get category
+	@GetMapping("/{catId}")
+	public ResponseEntity<CategoryDto> getCategory(@PathVariable("catId") int catId){
+		CategoryDto category = catServices.getCategoryById(catId);
+		return ResponseEntity.ok(category);
+	}
+	
+
+	//get All categories
+	@GetMapping("/all")
+	public ResponseEntity<List<CategoryDto>> getAllCategory(){
+		List<CategoryDto> allCategory = catServices.getAllCategory();
+		return ResponseEntity.ok(allCategory);
 	}
 	
 	
